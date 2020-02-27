5c6e1ea1c9c897b464e090a9c2bfb4a55c0613eb
Eugene Shershov <Absolute@tut.by>
2020-02-27 09:34:54 +0300

    feat: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+    let str = String(n).split('').reverse().join('');
+    let num = Number(str);
+    return num;
 }
